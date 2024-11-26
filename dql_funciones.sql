935d414b0ff35fdfc473607277a9e1e2d958d9b1
luismiguel <luismiguel110205@gmail.com>
2024-11-26 12:59:23 -0500

    feat: :sparkles: 2 functions completed

---
+++ dql_funciones.sql
@@ -0,0 +1,30 @@
+use finca_agricola_LL;
+
+-- precio total de una compra
+drop function if exists precio_total;
+delimiter //
+create function precio_total (id_compra_i int)
+returns bigint deterministic
+begin
+	declare precio_total bigint;
+    set precio_total = (select sum(precio_unitario * cantidad) from compra_producto where id_compra = id_compra_i group by id_compra);
+    return precio_total;
+end
+// delimiter ;
+
+select precio_total (2);
+select * from compra_producto;
+
+-- producto mas caro de una compra
+drop function if exists ver_producto_caro;
+delimiter //
+create function ver_producto_caro (id_compra_i int)
+returns varchar(50) deterministic
+begin
+	declare producto_i varchar(50);
+    set producto_i = (select producto from compra_producto where id_compra = id_compra_i order by precio_unitario desc limit 1);
+    return producto_i;
+end
+// delimiter ;
+
+select ver_producto_caro (6);
