f40dee9f8aa0dc8772e97628feedef3f147f3992
luis miguel <luismiguelcaicedobermon@gmail.com>
2024-11-27 01:38:41 -0500

    feat: :sparkles: add procedures

--- dql_procedimientos.sql
+++ dql_procedimientos.sql
@@ -62,3 +62,50 @@ end
 
 call asignar_salarios();
 
+
+-- 5 hacer una venta 
+drop procedure if exists hacer_venta;
+delimiter //
+create procedure hacer_venta (
+    fecha_venta date,
+    id_cliente int,
+    id_metodo_pago int
+)
+begin 
+	insert into venta(fecha_venta,id_cliente,precio_total,id_metodo_pago) values (fecha_venta,id_cliente,null,id_metodo_pago);
+end
+//delimiter ;
+
+call hacer_venta (current_date(),4,2);
+select * from venta;
+
+-- 6 asignar productos a una venta
+drop procedure if exists asignar_productos_venta;
+delimiter //
+create procedure asignar_productos_venta (
+	in id_venta int,
+    in id_presentacion_productoF int,
+    in cantidad int,
+    out mensaje varchar(70)
+)
+begin 
+	declare cantidad_kg_vender int;
+	declare cantidad_kg_disponibles int;
+    declare id_producto int;
+    set id_producto = (select id_producto from presentacion_producto where id = id_presentacion_productoF);
+    set cantidad_kg_disponibles = (select cantidad_kg from producto where id = id_producto);
+    set cantidad_kg_vender = (select cantidad_kg from presentacion_producto where id = id_presentacion_productoF) * cantidad;
+    
+	if (cantidad_kg_vender < cantidad_kg_disponibles) then
+		insert into venta_producto(id_venta,id_presentacion_producto,cantidad) values (id_venta,id_presentacion_producto,cantidad);
+        update producto set cantidad_kg = cantidad_kg_disponibles - cantidad_kg_vender where id = id_producto;
+        select 'el producto fue agregado' into mensaje;
+	else select 'la cantida de producto que deseas agregar supera la cantidad en stok' into mensaje;
+    end if;
+end
+//delimiter ;
+
+
+
+call asignar_productos_venta(51,2,1,@mensaje);
+select @mensaje;
